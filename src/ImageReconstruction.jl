9fa1b58dd8909f7e2959ac30475a8bc574388680
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2020-02-26 10:00:51 -0600

    a pixel-driven radon transform

--- src/ImageReconstruction.jl
+++ src/ImageReconstruction.jl
@@ -1,5 +1,7 @@
 module ImageReconstruction
 
+export radon, iradon
+
 """
 radon transform
 
@@ -14,6 +16,29 @@ https://en.wikipedia.org/wiki/Radon_transform
 """
 iradon
 
-include("pixel_driven.jl")
+
+"""
+Radon transform of an image using a pixel-driven algorithm.
+"""
+function radon(image::AbstractMatrix, θ::AbstractRange, t::AbstractRange)
+    P = zeros(eltype(image), length(t), length(θ))
+    nr, nc = size(image)
+
+    for i = 1:nr, j = 1:nc, (k, θₖ) in enumerate(θ)
+        x = j - nc / 2 + 0.5
+        y = i - nr / 2 + 0.5
+        t′ = x * cos(θₖ) + y * sin(θₖ)
+
+        #TODO this could probably be changed to O(1)
+        #TODO need to bounds check a and a+1
+        a = findfirst(x -> abs(x - t′) < step(t), t)
+
+        α = abs(t′ - t[a])
+        P[a, k] += (1 - α) * image[i, j]
+        P[a+1, k] += α * image[i, j]
+    end
+
+    P
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using ImageReconstruction
 using Test
 
-@testset "ImageReconstruction.jl" begin
-    # Write your own tests here.
+@testset "radon: pixel-driven" begin
+    pixels = 129
+    views = 200
+    I = zeros(pixels, pixels)
+    I[pixels÷2, pixels÷2] = 1
+    θ = range(0, step=2π/views, length=views)
+    t = -100:100
+
+    P = radon(I, θ, t)
+
+    @test all(P[101, :] .== 1)
 end
